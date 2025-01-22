a0f01c6fdecc599cf0456b08f76240500715ba88
Neural Falcon <neuralfalconofficial@gmail.com>
2025-01-22 22:56:47 +0530

    Update api.py

--- api.py
+++ api.py
@@ -5,22 +5,25 @@ import os
 from gradio_client import Client
 
 # Ensure the output directory exists
-output_dir = "temp_audio"
+output_dir = "api_output"
 os.makedirs(output_dir, exist_ok=True)
 
 # Initialize the Gradio client
 api_url = "http://127.0.0.1:7860/"
 client = Client(api_url)
 
+
+
+
 def text_to_speech(
     text="Hello!!",
     model_name="kokoro-v0_19.pth",
     voice_name="af_bella",
     speed=1,
-    trim=0,
     pad_between_segments=0,
     remove_silence=False,
     minimum_silence=0.05,
+    custom_voicepack=None,
 ):
     """
     Generates speech from text using a specified model and saves the audio file.
@@ -30,10 +33,10 @@ def text_to_speech(
         model_name (str): The name of the model to use for synthesis.
         voice_name (str): The name of the voice to use.
         speed (float): The speed of speech.
-        trim (int): Whether to trim silence at the beginning and end.
         pad_between_segments (int): Padding between audio segments.
         remove_silence (bool): Whether to remove silence from the audio.
         minimum_silence (float): Minimum silence duration to consider.
+        custom_voicepack (str): Path to the custom voice pack to use.
     Returns:
         str: Path to the saved audio file.
     """
@@ -43,13 +46,12 @@ def text_to_speech(
         model_name=model_name,
         voice_name=voice_name,
         speed=speed,
-        trim=trim,
         pad_between_segments=pad_between_segments,
         remove_silence=remove_silence,
         minimum_silence=minimum_silence,
+        custom_voicepack=custom_voicepack,
         api_name="/text_to_speech"
     )
-
     # Save the audio file in the specified directory
     save_at = f"{output_dir}/{os.path.basename(result)}"
     shutil.move(result, save_at)
@@ -63,13 +65,12 @@ if __name__ == "__main__":
     model_name="kokoro-v0_19.pth" #kokoro-v0_19-half.pth
     voice_name="af_bella" #get voice names 
     speed=1
-    only_trim_both_ends_silence=0
     add_silence_between_segments=0 #it use in large text
     remove_silence=False
     keep_silence_upto=0.05 #in seconds
+    custom_voicepack=None
     audio_path = text_to_speech(text=text, model_name=model_name, 
                                 voice_name=voice_name, speed=speed, 
-                                trim=only_trim_both_ends_silence,
                                 pad_between_segments=add_silence_between_segments,
                                 remove_silence=remove_silence,
                                 minimum_silence=keep_silence_upto)
